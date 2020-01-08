55c78466f381d18552c2499ebe300bae6cc70d6d
Daniel Rogers <Daniel.Rogers@enable.com>
2020-01-08 11:43:55 +0000

    Add SignalRChat app

---
+++ SignalRChat/Api/Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Api.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendToAll(string name, string message)
+        {
+            await Clients.All.SendAsync("sendToAll", name, message);
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public async Task SendToGroup(string name, string message, string group)
+        {
+            await Clients.Groups(group).SendAsync(group, name, message);
+        }
+    }
+}
