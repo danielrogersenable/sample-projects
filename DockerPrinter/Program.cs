c317105975f2a80b0b781e763fc6e12f1972d6f3
Daniel Rogers <daniel.rogers@enable.com>
2021-01-12 16:34:58 +0000

    Add basic Docker application

---
+++ DockerPrinter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DockerPrinter
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var timer = DateTimeOffset.Now;
+
+            var counter = 0;
+            var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+            while (max == -1 || counter < max)
+            {
+                Console.WriteLine($"Counter: {++counter}");
+                await Task.Delay(counter);
+            }
+
+            Console.WriteLine($"Total duration (ms): {(DateTimeOffset.Now - timer).TotalMilliseconds}");
+        }
+    }
+}
